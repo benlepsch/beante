27b00467d32174babcd29afad61a080453749ede
benlepsch <blepsch@gmail.com>
2020-03-06 08:46:14 -0500

    text entry and resizing window and scrollable

--- beante.py
+++ beante.py
@@ -1,1 +1,17 @@
 from tkinter import *
+
+def getFileValue():
+    return text.get('1.0', 'end-1c')
+
+root = Tk()
+root.title('bean text editor')
+
+scroll = Scrollbar(root)
+scroll.pack(side=RIGHT, fill=Y)
+
+text = Text(root, wrap=NONE, yscrollcommand=scroll.set)
+text.pack(side=LEFT, expand=1, fill='both')
+
+scroll.config(command=text.yview)
+
+root.mainloop()
