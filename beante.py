8f249125a8eebe75e3f6eacc0c07683bef48536d
benlepsch <blepsch@gmail.com>
2020-03-06 12:45:44 -0500

    add placeholder buttons

--- beante.py
+++ beante.py
@@ -6,11 +6,21 @@ def getFileValue():
 root = Tk()
 root.title('bean text editor')
 
+top = Frame(root)
+bottom = Frame(root)
+top.pack(side=TOP)
+bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
 scroll = Scrollbar(root)
-scroll.pack(side=RIGHT, fill=Y)
+scroll.pack(in_=bottom, side=RIGHT, fill=Y)
+
+button = Button(root, text='toggle dark mode', command=getFileValue)
+button.pack(in_=top, side=LEFT)
+button2 = Button(root, text='or not', command=getFileValue)
+button2.pack(in_=top, side=LEFT)
 
 text = Text(root, wrap=NONE, yscrollcommand=scroll.set)
-text.pack(side=LEFT, expand=1, fill='both')
+text.pack(in_=bottom,side=LEFT, expand=1, fill='both')
 
 scroll.config(command=text.yview)
 
